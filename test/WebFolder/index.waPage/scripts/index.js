adb24114a116c1cb5abaf24fcb53f194fb5afcf4
Bill McCarvell <bill@syntaxis.net>
2013-04-20 01:10:16 -0400

    Update #3

---
+++ test/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,19 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		debugger;
+		var vTest = "hey";
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
